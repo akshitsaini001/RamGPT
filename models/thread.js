9558feb366aed7e69e5be097777a6d6fcf6e4358
Akshit Saini <akshitsaini75000@gmail.com>
2026-05-24 13:03:42 +0530

    Add models folder and define thread schema

---
+++ models/thread.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+    role:{
+        type:String,
+        enum: ["user" , "assistant"],
+        required: true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    timeStamp:{
+        type:Date,
+        default: Date.now
+    }
+})
+
+const ThreadSchema = new mongoose.Schema({
+    usrerId:{
+        type:String,
+        required:true
+    },
+    threadId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    title:{
+        type:String,
+        default: "New Chat"
+    },
+    messages:[MessageSchema],
+    createdAt:{
+        type:Date,
+        default: Date.now
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const Thread = mongoose.model('Thread', ThreadSchema);
+export default Thread;
